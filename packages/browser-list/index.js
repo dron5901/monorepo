ca0d454563d26a7d58cdf72b2aaad998ae231e07
Andrew Vashchilin <avashchilin@prooftechit.com>
2023-12-13 20:47:07 +0300

    feat(bl): chrome 115

--- packages/browser-list/index.js
+++ packages/browser-list/index.js
@@ -1,1 +1,1 @@
-module.exports = ['Chrome >= 108', 'Firefox >= 100', 'Edge >= 107', 'Safari > 15.6', 'iOS >= 15.6'];
+module.exports = ['Chrome >= 115', 'Firefox >= 100', 'Edge >= 107', 'Safari > 15.6', 'iOS >= 15.6'];
